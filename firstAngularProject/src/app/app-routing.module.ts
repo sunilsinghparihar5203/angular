7f5c424c392c3e2b4bbdc24739e692b21c8b1025
sunil singh parihar <sunilsinghparihar5203@gmail.com>
2023-06-13 09:16:31 +0000

    event handing , and loops

--- firstAngularProject/src/app/app-routing.module.ts
+++ firstAngularProject/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:MainComponent},
+  {path:"about",component:AboutComponent},
+  {path:"contact",component:ContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- firstAngularProject/src/app/app.module.ts
+++ firstAngularProject/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AboutComponent,
+    ContactComponent,
+    FooterComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
